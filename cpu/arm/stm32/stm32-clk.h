78e00bdac48c2af22a6d8439d385f56f7da313d1
Erik Jansson <erik@upwis.com>
2012-02-16 00:34:29 +0100

    [STM32] Add the stm32 cpu directory where stuff common for STM32 architectures can live. Initially, only the stm32-clk module resides there.
    This is the idea:
    There are a few variants of the STM32 (for example STM32F1xx, STM32F2xx,
    STM32F4xx, STM32Lxxx, etc). They share many internal peripherals, like
    USART, SPI, I2C, etc. Code for these peripherals could be shared among
    Contiki implementations.
    
    The clock module can hopefully be used to make the devices reusable
    independent of what type of bus and clock is driving the device.

---
+++ cpu/arm/stm32/stm32-clk.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2012, UPWIS AB, Erik Jansson (erik at upwis.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+#ifndef STM32_CLK_H
+#define STM32_CLK_H
+
+#include <stdint.h>
+
+#define KHZ (1000)
+#define MHZ (1000 * KHZ)
+
+typedef uint32_t (*stm32_clk_freq_fn)(void);
+typedef void (*stm32_clk_enable_fn)(int);
+
+typedef struct {
+  stm32_clk_freq_fn freq;
+  stm32_clk_enable_fn enable;
+} stm32_clk;
+
+uint32_t stm32_clk_frequency(stm32_clk clk);
+uint32_t stm32_clk_enable(stm32_clk clk);
+uint32_t stm32_clk_disable(stm32_clk clk);
+
+/* An STM32 port should implement this. Given a peripheral base address,
+   it should return an stm32_clk. */
+stm32_clk stm32_clk_arch_clkof(void *periph);
+stm32_clk stm32_clk_clkof(void *periph);
+
+#endif
