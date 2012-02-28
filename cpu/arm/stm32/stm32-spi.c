88bed553817d4bc8a7a46b35ee7bdcda78e8537e
Erik Jansson <erik@upwis.com>
2012-02-28 14:18:57 +0100

    [STM32] Fix SPI bugs (rate calculations and transmission)

--- cpu/arm/stm32/stm32-spi.c
+++ cpu/arm/stm32/stm32-spi.c
@@ -53,29 +53,41 @@ void stm32_spi_init(STM32_SPI *spi)
 
 /**
    Set the highest rate possible given a maximum rate using the rate
-   of the clock clocking the SPI peripheral and the divisor.  
+   of the clock clocking the SPI peripheral and the divisor.
 */
-void 
+void
 stm32_spi_set_rate(STM32_SPI *spi, uint32_t max_rate)
 {
   uint32_t clkdiv;
   uint32_t actual_rate;
   uint32_t pclk_rate;
+  uint32_t enabled;
+
+  /* Check if SPI is enabled, if so disable first */
+  enabled = spi->CR1 & SPI_CR1_SPE;
+  if (enabled) {
+    stm32_spi_disable(spi);
+  }
 
   pclk_rate = stm32_clk_frequency(stm32_clk_clkof(spi));
-  clkdiv = 7;
+  clkdiv = 0;
 
   do {
-    clkdiv--;
-    actual_rate = pclk_rate / (1 << (clkdiv + 1));
-  } while (max_rate > actual_rate && clkdiv > 0);
+    clkdiv++;
+    actual_rate = pclk_rate / (1 << clkdiv);
+  } while (max_rate < actual_rate && (clkdiv) < 8);
 
-  printf("SPI rate %lu Hz (max %lu Hz)\n", 
-         actual_rate, max_rate);
+  printf("SPI rate: %lu Hz (device max: %lu Hz), clkdiv: %lu\n",
+         actual_rate, max_rate, clkdiv);
 
   /* Set new rate */
   spi->CR1 &= ~(SPI_CR1_BR);
-  spi->CR1 |= clkdiv;
+  spi->CR1 |= ((clkdiv << 3) & SPI_CR1_BR);
+
+  /* If SPI was enabled before, enable it again */
+  if (enabled) {
+    stm32_spi_enable(spi);
+  }
 }
 
 void 
@@ -89,7 +101,6 @@ stm32_spi_rx(STM32_SPI *spi)
 {
   while(!(spi->SR & SPI_SR_RXNE))
     ;
-  
   return spi->DR;
 }
 
@@ -98,14 +109,13 @@ stm32_spi_tx(STM32_SPI *spi, uint16_t data)
 {
   while(!(spi->SR & SPI_SR_TXE))
     ;
-
   spi->DR = data;
 }
 
 uint16_t
 stm32_spi_txrx(STM32_SPI *spi, uint16_t data)
 {
-  stm32_spi_tx(spi, data);
+  (void)stm32_spi_tx(spi, data);
 
   return stm32_spi_rx(spi);
 }
