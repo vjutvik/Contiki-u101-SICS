7ae9f83e16f839d2ba71e04d22f9560b6ac618f5
Erik Jansson <erik@upwis.com>
2012-06-14 08:57:12 +0200

    [U101-STM32F] Add platform-conf.h

---
+++ platform/u101-stm32f/platform-conf.h
@@ -0,0 +1,19 @@
+#ifndef PLATFORM_CONF_H
+#define PLATFORM_CONF_H
+
+#include <stdint.h>
+#include "uspi.h"
+#include "ui2c.h"
+#include "stm32-spi.h"
+
+extern const uspi_master spim1;
+extern const uspi_master spim2;
+
+extern const uspi_device rf230_spi;
+
+extern const ui2c_master i2cm1;
+
+extern const ui2c_slave lsm303dlh_i2c;
+extern const ui2c_slave stcn75_i2c;
+
+#endif
