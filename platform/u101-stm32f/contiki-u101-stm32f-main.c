bda8e786dc40837c17ff94a4b7ed5ad7cd1edcca
Erik Jansson <erik@upwis.com>
2012-06-14 09:05:26 +0200

    [U101-STM32F] Fix build problems due to changes in stm32-clk.

--- platform/u101-stm32f/contiki-u101-stm32f-main.c
+++ platform/u101-stm32f/contiki-u101-stm32f-main.c
@@ -56,10 +56,10 @@ u101_stm32f_banner(void)
 
   printf("\n\nInitialising %s\n", CONTIKI_VERSION_STRING);
   printf("Platform u101-stm32f (dev %04x, rev %04x)\n", device, revision);
-  printf("SYSCLK:  %lu %s\n", stm32_clk_frequency(sys_clk), mhz);
-  printf("AHBCLK:  %lu %s\n", stm32_clk_frequency(ahb_clk), mhz);
-  printf("APBCLK1: %lu %s\n", stm32_clk_frequency(apb1_clk), mhz);
-  printf("APBCLK2: %lu %s\n", stm32_clk_frequency(apb2_clk), mhz);
+  printf("SYSCLK:  %lu %s\n", stm32_clk_frequency(&sys_clk), mhz);
+  printf("AHBCLK:  %lu %s\n", stm32_clk_frequency(&ahb_clk), mhz);
+  printf("APBCLK1: %lu %s\n", stm32_clk_frequency(&apb1_clk), mhz);
+  printf("APBCLK2: %lu %s\n", stm32_clk_frequency(&apb2_clk), mhz);
   printf("UID96:       %04x %04x %04x %04x %04x %04x\n", 
          uid96[0], uid96[1], uid96[2], uid96[3], uid96[4], uid96[5]);
   printf("PseudoID16:  %04x\n", pseudoid16);
